6f2e80e0a7dfee159bea1b16c2d9c4119541259f
Duhan07 <gnsl.duhan.07@gmail.com>
2025-02-17 12:17:00 +0300

    Add debug logging for API base URLs

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,6 +3,9 @@ import axios from "axios";
 export const BranchAPI = axios.create({
   baseURL: import.meta.env.VITE_APP_BRANCH_API_URL,
 });
+console.log(import.meta.env.VITE_APP_BRANCH_API_URL)
+
+console.log({BranchAPI})
 
 export const BuildingsAPI = axios.create({
   baseURL: import.meta.env.VITE_APP_BUILDINGS_API_URL,
